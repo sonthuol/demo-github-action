521dd805a984293e381bc9c8646a799d38006007
sonthuol <sonthuolb3@gmail.com>
2023-12-26 22:30:23 +0700

    add test case

--- src/test/java/com/github/action/GitHubActionApplicationTests.java
+++ src/test/java/com/github/action/GitHubActionApplicationTests.java
@@ -1,5 +1,6 @@
 package com.github.action;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,11 @@ class GitHubActionApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void isTrue(){
+		Assertions.assertTrue(true);
 	}
 
 }
